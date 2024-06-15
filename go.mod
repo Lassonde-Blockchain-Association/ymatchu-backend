3fa63d3982ddba0d6c80a56141ac7650ba2c6dbf
samsonc <samsonc0514@gmail.com>
2024-06-15 10:22:38 -0400

    added with init and postgres set up

--- go.mod
+++ go.mod
@@ -3,21 +3,23 @@ module github.com/Lassonde-Blockchain-Association/ymatchu-backend
 go 1.22.4
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.6.4 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+	gorm.io/driver/postgres v1.5.9
+	gorm.io/gorm v1.25.10
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.0.2 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
-	github.com/jackc/pgtype v1.4.2 // indirect
-	github.com/jackc/pgx/v4 v4.8.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.2 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
-	gorm.io/driver/postgres v1.0.0 // indirect
-	gorm.io/gorm v1.21.12 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
 
-	uuid "github.com/google/uuid"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type LandlordDetails struct {
@@ -52,5 +61,98 @@ type Location struct {
 
 type PropertyImage struct {
     ListingID uuid.UUID `gorm:"type:uuid"`
-    MediaURL  []string  `gorm:"type:text[]"`
+    ImageURL  []string  `gorm:"type:text[]"`
+    VideoURL  []string  `gorm:"type:text[]"`
+}
+
+var db *gorm.DB
+
+
+func initDB() {
+    // loading environment variables 
+    if err := godotenv.Load(); err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+
+    // build the DSN from .env 
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_NAME"),
+        os.Getenv("DB_PORT"),
+    )
+
+    var err error
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+    }
+
+    db.AutoMigrate(&LandlordDetails{}, &Listing{}, &Utilities{}, &Features{}, &Location{}, &PropertyImage{})
+    log.Println("Database migrated successfully!")
+}
+
+
+type ListingDetails struct {
+	Listing     Listing
+	Utilities   Utilities
+	Features    Features
+	Location    Location
+	PropertyImages []PropertyImage
+}
+
+func createListingDetails(w http.ResponseWriter, r *http.Request) {
+	var listingDetails ListingDetails
+	if err := json.NewDecoder(r.Body).Decode(&listingDetails); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Create the Listing
+	if err := db.Create(&listingDetails.Listing).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set the ListingID for the related entities
+	listingID := listingDetails.Listing.ID
+	listingDetails.Utilities.ListingID = listingID
+	listingDetails.Features.ListingID = listingID
+	listingDetails.Location.ListingID = listingID
+	for i := range listingDetails.PropertyImages {
+		listingDetails.PropertyImages[i].ListingID = listingID
+	}
+
+	// Create related entities
+	if err := db.Create(&listingDetails.Utilities).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := db.Create(&listingDetails.Features).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := db.Create(&listingDetails.Location).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := db.Create(&listingDetails.PropertyImages).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(listingDetails)
+}
+
+func main() {
+    // initialization
+    initDB()
+
+    // create a new router
+    r := mux.NewRouter()
+
+	r.HandleFunc("/listings", createListingDetails).Methods("POST")
+
+    log.Fatal(http.ListenAndServe(":8000", r))
 }
